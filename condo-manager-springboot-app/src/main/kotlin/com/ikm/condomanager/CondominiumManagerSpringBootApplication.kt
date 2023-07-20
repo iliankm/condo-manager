5fd65bc1327bbca6f5e1933bb4effde613317bc5
Iliyan Kamilov <ilian.kamilov@gmail.com>
2023-07-20 20:00:38 +0300

    Introduce condo-manager-springboot-app parent module

---
+++ condo-manager-springboot-app/src/main/kotlin/com/ikm/condomanager/CondominiumManagerSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.ikm.condomanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CondominiumManagerSpringBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<CondominiumManagerSpringBootApplication>(args = args)
+}
---
+++ condo-manager-springboot-app/src/test/kotlin/com/ikm/condomanager/CondominiumManagerSpringBootApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.ikm.condomanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CondominiumManagerSpringBootApplicationTests {
+
+    @Test
+    fun contextLoads() = Unit
+
+}
