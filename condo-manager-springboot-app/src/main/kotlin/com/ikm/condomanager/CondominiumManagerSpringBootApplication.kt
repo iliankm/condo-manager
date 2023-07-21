b64d86434e9af480dee30aae84c21c5079cd392e
Iliyan Kamilov <ilian.kamilov@gmail.com>
2023-07-21 11:59:29 +0300

    configure detekt-formatting in IntelliJ and maven

--- condo-manager-springboot-app/src/main/kotlin/com/ikm/condomanager/CondominiumManagerSpringBootApplication.kt
+++ condo-manager-springboot-app/src/main/kotlin/com/ikm/condomanager/CondominiumManagerSpringBootApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class CondominiumManagerSpringBootApplication
 
 fun main(args: Array<String>) {
-	runApplication<CondominiumManagerSpringBootApplication>(args = args)
+    runApplication<CondominiumManagerSpringBootApplication>(args = args)
 }
--- condo-manager-springboot-app/src/test/kotlin/com/ikm/condomanager/CondominiumManagerSpringBootApplicationTests.kt
+++ condo-manager-springboot-app/src/test/kotlin/com/ikm/condomanager/CondominiumManagerSpringBootApplicationTests.kt
@@ -5,8 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class CondominiumManagerSpringBootApplicationTests {
-
     @Test
     fun contextLoads() = Unit
-
 }
