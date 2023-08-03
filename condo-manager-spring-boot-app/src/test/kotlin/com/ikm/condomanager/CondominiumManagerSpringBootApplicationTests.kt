9839544f1af363e3b07114309ec6cd574150afb7
Iliyan Kamilov <ilian.kamilov@gmail.com>
2023-08-03 18:52:50 +0300

    Introduce condo-manager-adapter-persistence module

--- condo-manager-spring-boot-app/src/test/kotlin/com/ikm/condomanager/CondominiumManagerSpringBootApplicationTests.kt
+++ condo-manager-spring-boot-app/src/test/kotlin/com/ikm/condomanager/CondominiumManagerSpringBootApplicationTests.kt
@@ -2,8 +2,10 @@ package com.ikm.condomanager
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CondominiumManagerSpringBootApplicationTests {
     @Test
     fun contextLoads() = Unit
