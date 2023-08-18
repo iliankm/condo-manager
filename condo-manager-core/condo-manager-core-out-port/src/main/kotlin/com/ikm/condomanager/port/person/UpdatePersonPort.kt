ee8124054b1c03ee93a6e2df096871fed0b3acca
Iliyan Kamilov <ilian.kamilov@gmail.com>
2023-08-18 14:45:53 +0300

    fix KDoc in UpdatePersonPort

--- condo-manager-core/condo-manager-core-out-port/src/main/kotlin/com/ikm/condomanager/port/person/UpdatePersonPort.kt
+++ condo-manager-core/condo-manager-core-out-port/src/main/kotlin/com/ikm/condomanager/port/person/UpdatePersonPort.kt
@@ -10,12 +10,13 @@ import com.ikm.condomanager.exception.VersionNotMatchedException
 interface UpdatePersonPort {
     /**
      * Updates the given valid [person] in the db.
-     * [Person.id] must be provided.
+     * [person#id] and [person#id#version] must be provided.
      *
-     * @param person the [Person] to be updated.
-     * @return the updated [Person].
-     * @throws NotFoundException if the [Person] to be updated, identified by [Person.id], is not found in the db.
-     * @throws VersionNotMatchedException if [Person.id?.version] is passed, but it doesn't match version of the object.
+     * @param person the [Person] to be updated
+     * @return the updated [Person]
+     * @throws IllegalStateException if [person#id] or [person#id#version] are not provided
+     * @throws NotFoundException if the [Person] to be updated, identified by [Person.id], is not found in the db
+     * @throws VersionNotMatchedException if [person#id#version] doesn't match the actual version of the object
      */
     fun update(person: Person): Person
 }
