789c06012b2ba14d9f7625f0c5f3757fae1668fd
Iliyan Kamilov <ilian.kamilov@gmail.com>
2023-12-12 16:58:01 +0200

    GH-62 EchoControllerTest improvement

--- condo-manager-infrastructure/condo-manager-infrastructure-spring-configuration/src/test/kotlin/com/ikm/condomanager/infra/controller/EchoControllerTest.kt
+++ condo-manager-infrastructure/condo-manager-infrastructure-spring-configuration/src/test/kotlin/com/ikm/condomanager/infra/controller/EchoControllerTest.kt
@@ -1,9 +1,8 @@
 package com.ikm.condomanager.infra.controller
 
-import com.ikm.condomanager.domain.AuthenticatedUser
 import com.ikm.condomanager.domain.Role
-import com.ikm.condomanager.port.user.LoadCurrentUserPort
-import com.ninjasquad.springmockk.MockkBean
+import com.ikm.condomanager.infra.configuration.SecurityConfiguration
+import com.ikm.condomanager.infra.user.LoadCurrentUserAdapter
 import io.mockk.every
 import io.mockk.mockkStatic
 import io.mockk.verifyAll
@@ -11,6 +10,8 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.Import
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
@@ -26,74 +27,66 @@ import kotlin.test.Test
  * Spring WebMvc test for [EchoController].
  */
 @WebMvcTest
-@ContextConfiguration(classes = [EchoController::class, TestComponent::class])
-@ActiveProfiles("integration-test")
+@Import(SecurityConfiguration::class)
+@ContextConfiguration(classes = [EchoController::class, TestComponent::class, LoadCurrentUserAdapter::class])
+@ActiveProfiles("test-infra-spring")
 class EchoControllerTest {
-    @MockkBean
-    lateinit var testComponent: TestComponent
-
-    @MockkBean
-    lateinit var loadCurrentUserPort: LoadCurrentUserPort
-
     @Autowired
     lateinit var mvc: MockMvc
 
     @Test
     fun `should get ping`() {
-        // given
-        every { testComponent.test1() } returns Unit
-        every { testComponent.test2() } returns Unit
-        every { testComponent.test3() } returns Unit
         // when
         val result = mvc.perform(
-            get("/api/v1/test/ping").with(jwt())
+            get("/api/v1/test/ping").with(
+                jwt().authorities(
+                    SimpleGrantedAuthority("ROLE_${Role.CONDO_MANAGER_USER.value}"),
+                    SimpleGrantedAuthority("ROLE_${Role.CONDOMINIUM_READ.value}")
+                )
+            )
         )
         // then
         result.andExpect(status().isOk)
         result.andExpect(content().string("ping"))
-        verifyAll {
-            testComponent.test1()
-            testComponent.test2()
-            testComponent.test3()
-        }
     }
 
     @Test
     fun `should get current user`() {
-        // given
-        every { loadCurrentUserPort.load() } returns AuthenticatedUser(
-            username = "User",
-            email = "user@domain.com",
-            firstName = "John",
-            lastName = "Doe",
-            roles = setOf(Role.CONDO_MANAGER_USER)
-        )
         // when
         val result = mvc.perform(
-            get("/api/v1/test/user").with(jwt())
+            get("/api/v1/test/user").with(
+                jwt().authorities(
+                    SimpleGrantedAuthority("ROLE_${Role.CONDO_MANAGER_USER.value}")
+                )
+            )
         )
         // then
         result.andExpect(status().isOk)
-        result.andExpect(jsonPath("$.username").value("User"))
-        verifyAll {
-            loadCurrentUserPort.load()
-        }
+        result.andExpect(jsonPath("$.username").value("user"))
     }
 
     @Test
     fun `should get current user from MDC context`() {
         // given
-        mockkStatic(MDC::get)
-        every { MDC.get("user") } returns "User"
+        mockkStatic(MDC::put, MDC::get, MDC::remove)
+        every { MDC.put("user", "user") } returns Unit
+        every { MDC.get("user") } returns "user"
+        every { MDC.remove("user") } returns Unit
         // when
         val result = mvc.perform(
-            get("/api/v1/test/user-in-mdc").with(jwt())
+            get("/api/v1/test/user-in-mdc").with(
+                jwt().authorities(
+                    SimpleGrantedAuthority("ROLE_${Role.CONDO_MANAGER_USER.value}")
+                )
+            )
         )
         // then
         result.andExpect(status().isOk)
-        result.andExpect(content().string("User"))
+        result.andExpect(content().string("user"))
         verifyAll {
+            MDC.put("user", "user")
             MDC.get("user")
+            MDC.remove("user")
         }
     }
 }
